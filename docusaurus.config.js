788752ed5439ab9199733d0713ebd4558ac6cb8c
Capsfly <2018640800@qq.com>
2025-03-05 14:49:59 -0500

    add contact info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '美研mscs介绍',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Make application easier',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
